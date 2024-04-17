99119e690b20a9e9877f7c6b2f7bf121797edc46
Даниил <gnedenkov_d_qschool@qsoft.ru>
2024-04-17 15:07:07 +0300

    284456 / Создание модели Новости и интеграция списка новостей на главной странице

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function home()
     {
-        return view('pages.homepage');
+        $articles = Article::orderBy('published_at', 'desc')->take(3)->get();
+        return view('pages.homepage', ['articles' => $articles]);
     }
 
     public function about()
