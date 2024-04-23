c73283d342b1f65c2d6713238c7c219091a545e5
Даниил <gnedenkov_d_qschool@qsoft.ru>
2024-04-23 13:26:25 +0300

    284464 / Работа с коллекциями и хелперами

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Article;
 use App\Models\Car;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PageController extends Controller
 {
@@ -45,6 +46,47 @@ class PageController extends Controller
 
     public function clients()
     {
-        return view('pages.clients');
+        $cars = Car::with(['carEngine', 'carBody', 'carClass'])->get();
+
+        $averagePrice = $cars->avg->price;
+
+        $averageDiscountedPrice = $cars->whereNotNull('old_price')->avg('price');
+
+        $mostExpensiveModel = $cars->sortByDesc('price')->first();
+
+        $uniqueSalons = $cars->pluck('salon')->unique();
+
+        $sortedEngines = $cars->pluck('carEngine.name')->sort();
+
+        $sortedClassNames = $cars
+            ->pluck('carClass.name')
+            ->unique()
+            ->sort()
+            ->mapWithKeys(fn ($item) => [Str::slug($item) => $item]);
+        
+        $discountedModels = $cars
+            ->whereNotNull('old_price')
+            ->filter(fn ($car) => preg_match('/[56]/', $car->name . $car->kpp . $car->carEngine->name));
+        
+        $averagePricesByBodyType = $cars
+            ->each(function ($car) {
+                $car->old_price = $car->old_price ?: $car->price;
+            })
+            ->groupBy('carBody.name')
+            ->map(function ($car) {
+                return $car->avg('old_price');
+            })
+            ->sort();
+
+        return view('pages.clients', [
+            'averagePrice' => $averagePrice,
+            'averageDiscountedPrice' => $averageDiscountedPrice,
+            'mostExpensiveModel' => $mostExpensiveModel,
+            'uniqueSalons' => $uniqueSalons,
+            'sortedEngines' => $sortedEngines,
+            'sortedClassNames' => $sortedClassNames,
+            'discountedModels' => $discountedModels,
+            'averagePricesByBodyType' => $averagePricesByBodyType,
+        ]);
     }
 }
